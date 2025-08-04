db84ec110bdeecbb96a0dec4c84df5d0677e125d
Mac <mac@Macs-MacBook-Air.local>
2025-08-04 12:42:19 +0100

    agric_website/urls.py

--- agric_website/urls.py
+++ agric_website/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path('management/', include('management.urls')),
+    path('favicon.ico', lambda request: HttpResponse(status=204)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
